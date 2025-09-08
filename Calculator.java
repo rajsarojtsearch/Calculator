46ef7014dc0329adc0203bbc033e3ac20cba40df
raj saroj <rajsaroj7991@gmail.com>
2025-09-08 14:22:53 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+ class Calculator{
+          public static void subtract(){
+
+          int x=45;
+          int y=20;
+          int diff=x-y;
+          System.out.println("The difference of "+x+" and "y+y" is: "+diff);
+
+}
+public static void main(Strung[] args){
+          System.out.println("===Program Starts===");
+          add();
+          add();
+          add();
+          subtract();
+          multiply();
+          System.out.println("===Program Ends===");
+}
+
+public static void add(){
+         int x=45;
+         int y=20;
+         int sum=x+y;
+         System.out.println("The sum of "+x+" and "+y+" is: "+sum);
+}
+public static void multiply(){
+         int x=50;
+         int y=2;
+         int multi=x*y;
+         System.out.println("the sum of "+x+" and "+y+" is: "+multi);
+}
+}
